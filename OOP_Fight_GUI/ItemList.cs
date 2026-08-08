0643fdb87e5d3c69f821dd4f5b6ee7a5ce071e5e
shcherbynastanislav1-droid <shcherbynastanislav1@gmail.com>
2026-08-08 12:13:18 +0000

    +awp

--- OOP_Fight_GUI/ItemList.cs
+++ OOP_Fight_GUI/ItemList.cs
@@ -7,9 +7,17 @@ public class ItemList {
         baseKnife.Stat = p => p.AttackMax += 2;
         itemsList.Add(baseKnife);
 
-        Item military_helmet = new Item("military_helmet","+4 armor");
+        Item military_helmet = new Item("Military helmet","just helmet, +4 armor");
         military_helmet.Stat = p => p.Armor += 4;
         itemsList.Add(military_helmet);
+
+        Item gun = new Item("Gun","ТТ-33, +4 max attack");
+        gun.Stat = p => p.AttackMax += 4;
+        itemsList.Add(gun);
+
+        Item awp = new Item("Awp", "..., +100 max attack");
+        awp.Stat = p => p.AttackMax += 4;
+        itemsList.Add(awp);
         
         if(idItem < itemsList.Count)
         {
